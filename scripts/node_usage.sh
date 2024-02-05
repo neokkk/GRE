d92194ca06d2f4473050b731f318d4f1b7c52a98
neokkk <seven3126@gmail.com>
2024-02-05 16:51:56 +0900

    feat: script

--- scripts/node_usage.sh
+++ scripts/node_usage.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+root=$(echo "$0" | awk -F '/' '{print $1}')
+echo "$root"
+
 output_path=""
 datasets="books history fb osm"
 op_num=100
 table_size=1000000
 
 for dataset in $datasets; do
-  ../build/microbench --keys_file=../datasets/$dataset --read=0 --insert=0 --scan=1 --scan_num=100 \
+  "$root/build/microbench" --keys_file="$root/datasets/$dataset" --read=0 --insert=0 --scan=1 --scan_num=100 \
     --table_size=$table_size --init_table_ratio=1 --operations_num=$op_num --thread_num=1 --index=lipp
 done
