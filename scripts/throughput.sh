e6f301c75cada6f05eb70ea1e46136354dddcc3d
neokkk <seven3126@gmail.com>
2024-02-26 15:46:11 +0900

    feat: throughput script

--- scripts/throughput.sh
+++ scripts/throughput.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-root=$(echo "$0" | awk -F '/' '{print $1}')
+root=$(dirname $(dirname "$0"))
 echo "root: $root"
 
 output_path=""
-datasets="books history fb osm"
+datasets="books history face osm"
+inserts="0 0.5 1"
 op_num=1000000
 table_size=100000000
 
 for dataset in $datasets; do
-  "$root/build/microbench" --keys_file="$root/datasets/$dataset" --read=0 --insert=0 --scan=1 \
-    --table_size=$table_size --init_table_ratio=1 --operations_num=$op_num --thread_num=1 --index=lipp --latency_sample=true
+  for insert in $inserts; do
+    read=$(echo "1 - $insert" | bc)
+    "$root/build/microbench" --keys_file="$root/datasets/$dataset" --read=$read --insert=$insert \
+      --table_size=$table_size --init_table_ratio=0.5 --operations_num=$op_num --thread_num=1 --index=lipp --latency_sample=true --memory=true
+  done
 done
